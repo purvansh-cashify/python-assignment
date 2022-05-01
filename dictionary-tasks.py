0782d0a86e945fcd73734a2c35f547af9566f282
Purvansh Arora <purvansh.arora@cashify.in>
2022-05-01 15:53:21 +0530

    printing the value of key 'history' & converting list to dictionary

--- dictionary-tasks.py
+++ dictionary-tasks.py
@@ -2,16 +2,44 @@
 Tasks given under dictionary section in the assignment
 '''
 
+# Converting two lists into dictionaries
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+dict1 = {}
+
+for i, j in zip(keys, values):
+    dict1[i] = j
+
+print("Converting lists to dictionary -")
+print(dict1, "\n")
+
 # merging two python dictionaries
 
 dict1 = {"a": 1, "b": 2, "c": 3}
 dict2 = {"d": 3, "e": 2, "f": 1}
 
 dict3 = dict1 | dict2
+print("Merging two dictionaries -")
+print(dict3, "\n")
 
-print(dict3)
+# Print the value of key ‘history’ from the below dict
+
+sampleDict = {
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80
+            }
+        }
+    }
+}
+print("Print the value of key history - ")
+print(sampleDict['class']['student']['marks']['history'])
 
 # initialise dictionary with default values
 
 # delete a list of keys
-
