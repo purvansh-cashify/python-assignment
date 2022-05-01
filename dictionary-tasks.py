c43ee654c466d30f1aa0f8893b45d90795593210
Purvansh Arora <purvansh.arora@cashify.in>
2022-05-01 15:37:02 +0530

    merging two dictionaries done

---
+++ dictionary-tasks.py
@@ -0,0 +1,17 @@
+'''
+Tasks given under dictionary section in the assignment
+'''
+
+# merging two python dictionaries
+
+dict1 = {"a": 1, "b": 2, "c": 3}
+dict2 = {"d": 3, "e": 2, "f": 1}
+
+dict3 = dict1 | dict2
+
+print(dict3)
+
+# initialise dictionary with default values
+
+# delete a list of keys
+
