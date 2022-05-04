1235d21532c7bff5be02781a73ac6fc816e1db19
Purvansh Arora <purvansh.arora@cashify.in>
2022-05-04 15:55:09 +0530

    return new set of identical items from two sets

--- set-tasks.py
+++ set-tasks.py
@@ -1,8 +1,29 @@
 # Add a list of elements to a set
-sample_set = {"Yellow", "Orange", "Black"}
-sample_list = ["Blue", "Green", "Red"]
+def add_list():
+    sample_set = {"Yellow", "Orange", "Black"}
+    sample_list = ["Blue", "Green", "Red"]
 
-sample_set.update(sample_list)
+    sample_set.update(sample_list)
 
-print("Add a list of elements to a set - ")
-print(sample_set)
+    print("Add a list of elements to a set - ")
+    print(sample_set,"\n")
+
+# Return a new set of identical items from two sets
+def identical():
+    set1 = {10, 20, 30, 40, 50}
+    set2 = {30, 40, 50, 60, 70}
+    new_set = set()
+
+    for i, j in zip(set1, set2):
+        if i==j:
+            new_set.add(i)
+
+    print("Return new set of identical items from two sets -")
+    print(new_set,"\n")
+
+def
+
+
+if __name__ == '__main__':
+    add_list()
+    identical()
