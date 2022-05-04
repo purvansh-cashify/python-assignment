b478cd817b88b17e822503d4417c9a54a45e3e23
Purvansh Arora <purvansh.arora@cashify.in>
2022-05-04 16:40:59 +0530

    reverse a tuple

---
+++ tuple-tasks.py
@@ -0,0 +1,10 @@
+def reverse_tuple():
+    tuple1 = (10, 20, 30, 40, 50)
+    tuple1 = tuple1[::-1]
+
+    print("Reverse the tuple - ")
+    print(tuple1, '\n')
+
+
+if __name__ == '__main__':
+    reverse_tuple()
