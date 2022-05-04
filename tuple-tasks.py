202fafeb3c9488b270144096a518b6edbba87f4f
Purvansh Arora <purvansh.arora@cashify.in>
2022-05-04 17:43:41 +0530

    Unpack the tuple into 4 variables

--- tuple-tasks.py
+++ tuple-tasks.py
@@ -10,17 +10,30 @@ def access():
     tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
 
     print("Access value 20 from the tuple - ")
-    print(tuple1[1][1])
+    print(tuple1[1][1], "\n")
 
 
 def create():
     tuple1 = tuple([50])
 
     print("Create a tuple with single item 50 - ")
-    print(tuple1)
+    print(tuple1, "\n")
+
+
+def unpack():
+    tuple1 = (10, 20, 30, 40)
+
+    a, b, c, d = tuple1
+
+    print("Unpack the tuple into 4 variables - ")
+    print(a)
+    print(b)
+    print(c)
+    print(d, "\n")
 
 
 if __name__ == '__main__':
     reverse_tuple()
     access()
     create()
+    unpack()
