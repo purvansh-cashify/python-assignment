cb33358b08381d6df468cf30fdd924634cf222ef
Purvansh Arora <purvansh.arora@cashify.in>
2022-05-04 16:01:11 +0530

    unique items from two sets

--- set-tasks.py
+++ set-tasks.py
@@ -6,7 +6,8 @@ def add_list():
     sample_set.update(sample_list)
 
     print("Add a list of elements to a set - ")
-    print(sample_set,"\n")
+    print(sample_set, "\n")
+
 
 # Return a new set of identical items from two sets
 def identical():
@@ -15,15 +16,25 @@ def identical():
     new_set = set()
 
     for i, j in zip(set1, set2):
-        if i==j:
+        if i == j:
             new_set.add(i)
 
     print("Return new set of identical items from two sets -")
-    print(new_set,"\n")
+    print(new_set, "\n")
 
-def
+
+# Get Only unique items from two sets
+def unique():
+    set1 = {10, 20, 30, 40, 50}
+    set2 = {30, 40, 50, 60, 70}
+
+    new_set = set1.union(set2)
+
+    print("Unique items from two sets - ")
+    print(new_set, "\n")
 
 
 if __name__ == '__main__':
     add_list()
     identical()
+    unique()
