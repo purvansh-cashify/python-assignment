c4dcf9a9078ea5dc7d1eb1112c31882543ba8d1c
Purvansh Arora <purvansh.arora@cashify.in>
2022-05-05 13:39:25 +0530

    pattern

--- loop-tasks.py
+++ loop-tasks.py
@@ -5,6 +5,19 @@ def first_ten():
         print(num)
         num += 1
 
+    print("\n")
+
+
+def pattern():
+    print("Print the following pattern - ")
+    for i in range(1, 6):
+        for j in range(1, i + 1):
+            print(j, end='')
+        print("\n")
+
+
+
 
 if __name__ == "__main__":
     first_ten()
+    pattern()
