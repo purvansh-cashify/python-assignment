ff966ff4459d5742eb4cef261e3e2d3cba35312f
Purvansh Arora <purvansh.arora@cashify.in>
2022-05-05 12:43:45 +0530

    Print First 10 natural numbers using while loop

---
+++ loop-tasks.py
@@ -0,0 +1,10 @@
+def first_ten():
+    num = 1
+    print("Print First 10 natural numbers using while loop - ")
+    while num < 11:
+        print(num)
+        num += 1
+
+
+if __name__ == "__main__":
+    first_ten()
