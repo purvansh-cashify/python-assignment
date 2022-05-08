dfcb44e872098a2e06f80b8f10f485b921df8544
Purvansh Arora <purvansh.arora@cashify.in>
2022-05-08 23:48:14 +0530

    Return multiple values from a function

--- methods-tasks.py
+++ methods-tasks.py
@@ -2,6 +2,7 @@ def func():
     print("Create a function with default argument - ")
     print("This is a function. \n")
 
+
 def variable(*args):
     lst = [i for i in args]
     print("Create a function with variable length of arguments - ")
@@ -9,6 +10,15 @@ def variable(*args):
     return sum(lst)
 
 
+def calculation(a, b):
+    sm = a + b
+    diff = a - b
+    print("Return multiple values from a function - ")
+
+    return sm, diff
+
+
 if __name__ == "__main__":
     func()
     print("Sum - ", variable(20, 40))
+    print(calculation(50, 30))
