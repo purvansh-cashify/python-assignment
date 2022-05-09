f0e94a9862ddc61c78d6dedd24c56c2b2e667508
Purvansh Arora <purvansh.arora@cashify.in>
2022-05-09 21:38:57 +0530

    Accept a list of 5 float numbers as an input from the user

--- io-tasks.py
+++ io-tasks.py
@@ -1,7 +1,7 @@
 def user_input():
     print("Accept numbers from a user - ")
-    # num = int(input("Enter your value - "))
-    # print(num, "\n")
+    num = int(input("Enter your value - "))
+    print(num, "\n")
 
 
 def print_name():
@@ -19,8 +19,18 @@ def decimal(num):
     print('%.2f' % num)
 
 
+def list_accept():
+    lst = []
+    print("\nAccept a list of 5 float numbers as an input from the user - ")
+    for i in range(0, 5):
+        lst.append(float(input()))
+
+    print(lst)
+
+
 if __name__ == "__main__":
-    user_input()
+    #user_input()
     print_name()
     octal(10)
     decimal(518.4567)
+    list_accept()
