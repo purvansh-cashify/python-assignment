52c4ab38479b299a6cd92bf6f4dde45cb4ffb5cb
Purvansh Arora <purvansh.arora@cashify.in>
2022-05-09 21:22:46 +0530

    Convert Decimal number to octal using print() output formatting

--- io-tasks.py
+++ io-tasks.py
@@ -8,8 +8,13 @@ def print_name():
     print(" Display three string “Name”, “Is”, “James” as “Name**Is**James” -")
     print('My', 'Name', 'Is', 'James', sep='**')
 
+def octal(num):
+    print("\nConvert Decimal number to octal using print() output formatting - ")
+    print('%o' % num)
+
 
 if __name__ == "__main__":
     user_input()
     print_name()
+    octal(10)
 
