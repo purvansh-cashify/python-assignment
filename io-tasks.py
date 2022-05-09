0dbd09dc36d00a29eff13dc8c9e691793a79e8b5
Purvansh Arora <purvansh.arora@cashify.in>
2022-05-09 21:20:24 +0530

    Display three string “Name”, “Is”, “James” as “Name**Is**James”

--- io-tasks.py
+++ io-tasks.py
@@ -1,8 +1,15 @@
 def user_input():
     print("Accept numbers from a user - ")
-    num = int(input("Enter your value - "))
-    print(num, "\n")
+    #num = int(input("Enter your value - "))
+    #print(num, "\n")
+
+
+def print_name():
+    print(" Display three string “Name”, “Is”, “James” as “Name**Is**James” -")
+    print('My', 'Name', 'Is', 'James', sep='**')
 
 
 if __name__ == "__main__":
     user_input()
+    print_name()
+
