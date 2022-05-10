355711d37bf1285e39d7b014685894758430df86
Purvansh Arora <purvansh.arora@cashify.in>
2022-05-10 13:18:04 +0530

    Create a string made of the middle three characters

--- string-tasks.py
+++ string-tasks.py
@@ -1,9 +1,15 @@
-
 def fml():
     str1 = "James"
-    str2 = str1[0] + str1[int(len(str1)/2)] +str1[-1]
+    str2 = str1[0] + str1[int(len(str1) / 2)] + str1[-1]
     print(str2)
 
+def mid():
+    str1 = "JhonDipPeta"
+    middle = int(len(str1)/2)
+    str2 = str1[middle-1] + str1[middle] + str1[middle+1]
+    print(str2)
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     fml()
+    mid()
+
