dfd0d757b9fc7831e5b1bad0b387f041ac9a78c9
Purvansh Arora <purvansh.arora@cashify.in>
2022-05-10 13:23:30 +0530

    Append new string in the middle of a given string

--- string-tasks.py
+++ string-tasks.py
@@ -9,7 +9,16 @@ def mid():
     str2 = str1[middle-1] + str1[middle] + str1[middle+1]
     print(str2)
 
+def new_string():
+    s1 = "Ault"
+    s2 = "Kelly"
+    middle = int(len(s1)/2)
+    s3 = s1[:middle] + s2 + s1[middle:]
+    print(s3)
+
+
 if __name__ == '__main__':
     fml()
     mid()
+    new_string()
 
