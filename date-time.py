3fcc89c8e8d48ba5a796da7c029922bb0a20640e
Purvansh Arora <purvansh.arora@cashify.in>
2022-05-10 11:50:47 +0530

    Convert string into a datetime object

--- date-time.py
+++ date-time.py
@@ -4,6 +4,13 @@ import datetime
 def get_time():
     print(datetime.datetime.now())
 
+def str_to_dt():
+    date_string = "Feb 25 2020 4:20PM"
+    datetime_object = datetime.datetime.strptime(date_string, '%b %d %Y %I:%M%p')
+    print(datetime_object)
+
 
 if __name__ == "__main__":
     get_time()
+    str_to_dt()
+
