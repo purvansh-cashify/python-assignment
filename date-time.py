a3e9a6a0126c0405a547f99b2bc4c00d63f631ca
Purvansh Arora <purvansh.arora@cashify.in>
2022-05-10 11:56:35 +0530

    Subtract a week (7 days)  from a given date in Python

--- date-time.py
+++ date-time.py
@@ -1,16 +1,23 @@
-import datetime
+from datetime import timedelta, datetime
 
 
 def get_time():
-    print(datetime.datetime.now())
+    print(datetime.now())
+
 
 def str_to_dt():
     date_string = "Feb 25 2020 4:20PM"
-    datetime_object = datetime.datetime.strptime(date_string, '%b %d %Y %I:%M%p')
+    datetime_object = datetime.strptime(date_string, '%b %d %Y %I:%M%p')
     print(datetime_object)
+
+
+def week_minus():
+    given_date = datetime(2020, 2, 25)
+    new_date = given_date - timedelta(days=7)
+    print(new_date)
 
 
 if __name__ == "__main__":
     get_time()
     str_to_dt()
-
+    week_minus()
